82ac55f40478471eaefab5a3fcc91a900668f779
Pavel Horcicka <pablo.alcudia@gmail.com>
2021-10-02 20:19:59 +0200

    2

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function initMap() {
   };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
+    zoom: 2,
     center: center,
   });
   // The marker, positioned at Uluru
